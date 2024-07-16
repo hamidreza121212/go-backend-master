2ed6f07381908dd37bc3f88c336f558698f8d32b
hamidreza121212 <www.hamidreza121212@gmail.com>
2024-07-16 18:33:12 +0330

    chor(db): write compelet test for entry model.

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"go-backend-master/painless/models/generator"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -33,4 +35,57 @@ func createRandomEntry(t *testing.T, account Account) Entry {
 func TestCreateEntry(t *testing.T) {
 	account := createRandomAccount(t)
 	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry_one := createRandomEntry(t, account)
+	entry_two, err := testQueries.GetEntry(context.Background(), entry_one.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry_two)
+
+	require.Equal(t, entry_one.ID, entry_two.ID)
+	require.Equal(t, entry_one.AccountID, entry_two.AccountID)
+	require.Equal(t, entry_one.Amount, entry_two.Amount)
+
+	require.WithinDuration(t, entry_one.CreatedAt, entry_two.CreatedAt, time.Second)
+
+}
+
+func TestDeleteEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry_one := createRandomEntry(t, account)
+
+	err := testQueries.DeleteEntry(context.Background(), entry_one.ID)
+	require.NoError(t, err)
+
+	entry_two, err := testQueries.GetEntry(context.Background(), entry_one.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry_two)
+
+}
+
+func TestListEntry(t *testing.T) {
+
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+
 }
